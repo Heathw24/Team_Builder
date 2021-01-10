22c2bf6b4972d7e8a919604cfc6a21121b693fd1
Heath Ward <heathw24@outlook.com>
2021-01-10 17:36:09 -0500

    set up questions and functions to take data from questions

--- app.js
+++ app.js
@@ -9,11 +9,149 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
+const { inherits } = require("util");
 
+var team = [];
+
+const roleQuestion = [
+    {
+        type: 'list',
+        name: 'role',
+        message: `What is the role of this team member?`,
+        choices: ["Manager", "Engineer", "Intern", "None/Done"],
+    }
+];
+
+const managerQuestions = [
+    {
+        type: 'input',
+        name: 'name',
+        message: 'What is the name of the manager?',
+    },
+
+    {
+        type: 'input',
+        name: 'id',
+        message: 'What is the ID of this manager?',
+    },
+
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is this managers email?',
+    },
+
+    {
+        type: 'input',
+        name: 'office',
+        message: 'What is this managers office number?',
+    },
+];
+
+const engineerQuestions = [
+    {
+        type: 'input',
+        name: 'name',
+        message: 'What is the name of the engineer?',
+    },
+
+    {
+        type: 'input',
+        name: 'id',
+        message: 'What is the ID of this engineer?',
+    },
+
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is this engineers email?',
+    },
+
+    {
+        type: 'input',
+        name: 'github',
+        message: 'What is this engineers github?',
+    },
+
+];
+
+const internQuestions = [
+    {
+        type: 'input',
+        name: 'name',
+        message: 'What is the name of the intern?',
+    },
+
+    {
+        type: 'input',
+        name: 'id',
+        message: 'What is the ID of this intern?',
+    },
+
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is this interns email?',
+    },
+
+    {
+        type: 'input',
+        name: 'school',
+        message: 'What is this interns school?',
+    },
+];
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
+function generateManager() {
+    inquirer.prompt(managerQuestions)
+      .then((answers) => {
+          const manager = new Manager(answers.name, answers.id, answers.email, answers.office);
+          team.push(manager);
+          init();
+      })
+};
 
+function generateEngineer() {
+    inquirer.prompt(engineerQuestions)
+      .then((answers) => {
+          const engineer = new Engineer(answers.name, answers.id, answers.email, answers.github);
+          team.push(engineer);
+          init();
+      })
+};
+
+function generateIntern() {
+    inquirer.prompt(internQuestions)
+      .then((answers) => {
+          const intern = new Intern(answers.name, answers.id, answers.email, answers.school);
+          team.push(intern);
+          init();
+      })
+};
+
+function init(){
+    inquirer.prompt(roleQuestion)
+    then((answer) => {
+        if(answer === "Manager"){
+            generateManager();
+        } 
+        else if(answer === "Engineer"){
+            generateEngineer();
+        }
+        else if(answer === "Intern" ){
+            generateIntern();
+        }
+        else {
+            console.log(team);
+            return;
+        }
+    })
+}
+
+    
+
+  init();
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
